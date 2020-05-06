120b6ddc41657a5e2b3d055653af69ef27aee34c
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-05-06 04:04:20 -0400

    Update youtubedownloader.py

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,13 +1,23 @@
 from pytube import YouTube
 
-def download_youtubevideo(url):
-    print("Creating YouTube Object from URL...")
-    yt = YouTube(url)
 
+
+def download_youtubevideo(URL):
+
+    """
+    This function takes a URL and leads the user through downloading it.
+    """
+
+    # Creates YouTube object
+    print("Creating YouTube Object from URL...")
+    yt = YouTube(URL)
+
+    # Displays the streams and itags from object for user-choosing
     print("Which itag would you like to Download?")
     print(yt.streams)
     streamindex = int(input("Select itag using array indexing: "))
 
+    # Downloads video based off user-chosen indexing
     print("Attempting to Download...")
     yt.streams[streamindex].download()
 
@@ -17,11 +27,3 @@ def download_youtubevideo(url):
 if __name__ == "__main__":
     url = input("Enter Youtube URL:")
     download_youtubevideo(url)
-
-
-
-
-
-
-
-
