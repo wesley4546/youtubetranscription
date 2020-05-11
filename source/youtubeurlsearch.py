339140338576cc5cdb1464950822c91bac3060b1
wesley4546 <wesley.gardiner4546@gmail.com>
2020-05-11 15:23:48 -0400

    Create youtubeurlsearch.py

---
+++ source/youtubeurlsearch.py
@@ -0,0 +1,17 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+textToSearch = 'corona information'
+query = urllib.parse.quote(textToSearch)
+url = "https://www.youtube.com/results?search_query=" + query
+response = urllib.request.urlopen(url)
+html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
+for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    if not vid['href'].startswith("https://googleads.g.doubleclick.net/"):
+        print('https://www.youtube.com' + vid['href'])
