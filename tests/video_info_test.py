3661d1002df8d4cc939369c5e5da084f503f8323
wesley4546 <wesley.gardiner4546@gmail.com>
2020-05-14 19:02:26 -0400

    Update video_info_test.py

--- tests/video_info_test.py
+++ tests/video_info_test.py
@@ -15,170 +15,52 @@ Excepted:
 All the keys should be filled in with the video information found on YouTube. 
 """
 
-url = "https://www.youtube.com/watch?v=GmiXyTckcHk"
 
-# Main program
-print("Testing Main Program's get_video_info() function on URL...")
-video_info = get_video_info(url)
-
-# Testing
-print("Testing altered get_video_info() function on URL...")
-try:
-
-    # requests URL
-    content = requests.get(url)
-
-    # create beautiful soup object to parse HTML
-    soup = bs(content.content, "html.parser")
-
-    # initialize the result
-    result = {}
-
-    # video title
-    try:
-        result['title'] = soup.find("span", attrs={"class": "watch-title"}).text.strip()
-    except:
-        result['title'] = "Not Found (Perhaps Hidden)"
-
-    # try-catch for finding video views using the HTML 'watch-view-count'
-    try:
-        # video views (converted to integer)
-        result['views'] = int(
-            soup.find("div", attrs={"class": "watch-view-count"}).text[:-6].replace(",", ""))
-    except:
-        try:
-            # Tries to find the views using the 'stat view-count'
-            result['views'] = int(
-                soup.find("span", attrs={"class": "stat view-count"}).text[:-6].replace(",", "").replace("views",
-                                                                                                         ""))
-        except:
-            # If views can't be found
-            result['views'] = "Not Found (Perhaps Hidden)"
-
-    # video description
-    try:
-        result['description'] = soup.find("p", attrs={"id": "eow-description"}).text
-    except:
-        result['description'] = "Not Found (Perhaps Hidden)"
-
-    # date published
-    try:
-        result['date_published'] = soup.find("strong", attrs={"class": "watch-time-text"}).text.replace(
-            "Published on ", "").replace("Premiered ", "")
-    except:
-        result['date_published'] = "Not Found (Perhaps Hidden)"
-
-    # try-catch for finding the likes and dislikes
-    try:
-        # number of likes as integer
-        result['likes'] = int(soup.find("button", attrs={"title": "I like this"}).text.replace(",", ""))
-
-        # number of dislikes as integer
-        result['dislikes'] = int(
-            soup.find("button", attrs={"title": "I dislike this"}).text.replace(",", ""))
-    except:
-        try:
-            # This took me so long to figure out. If you can find a better way PLEASE let me know
-            # Saves FULL html file into a variable
-            video_html = soup.prettify()
-
-            # pattern to extract html code that has the like count
-            pattern_like = re.compile(r'\\"likeCount\\":[0-9]+[0-9]')
-
-            # pattern to extract numbers our of like count
-            pattern_like2 = re.compile(r'[0-9]+[0-9]')
-
-            # Finds the html code with likecount
-            matches_in_html_like = pattern_like.findall(video_html)
-
-            # Extracts the numbers from the html code
-            cleaned_html_number_like = int((pattern_like2.findall(''.join(matches_in_html_like)))[0])
-
-            result['likes'] = cleaned_html_number_like
-
-            pattern_dislike = re.compile(r'\\"dislikeCount\\":[0-9]+[0-9]')
-            # pattern to extract numbers our of like count
-            pattern_dislike2 = re.compile(r'[0-9]+[0-9]')
-
-            # Finds the html code with dislikeCount
-            matches_in_html_dislike = pattern_dislike.findall(video_html)
-
-            # Extracts the numbers from the html code
-            cleaned_html_number_dislike = int((pattern_dislike2.findall(''.join(matches_in_html_dislike)))[0])
-
-            result['dislikes'] = cleaned_html_number_dislike
-        except:
-            result['likes'] = "Not Found (Perhaps Hidden)"
-            result['dislikes'] = "Not Found (Perhaps Hidden)"
-
-    # channel details
-    try:
-        channel_tag = soup.find("div", attrs={"class": "yt-user-info"}).find("a")
-    except:
-        channel_tag = "Not Found (Perhaps Hidden)"
-
-    # channel name
-    try:
-        channel_name = channel_tag.text
-    except:
-        channel_name = "Not Found (Perhaps Hidden)"
-
-    # channel URL
-    try:
-        channel_url = f"https://www.youtube.com{channel_tag['href']}"
-    except:
-        channel_url = "Not Found (Perhaps Hidden)"
-
-    # try-catch for subscription count (youtube user can hide these)
-    try:
-        channel_subscribers = soup.find("span", attrs={"class": "yt-subscriber-count"}).text.strip()
-    except:
-        channel_subscribers = "Not Found (Perhaps Hidden)"
-
-    # Combines all channel aspects
-    result['channel'] = {'name': channel_name, 'url': channel_url, 'subscribers': channel_subscribers}
+# Url to test
+url = "https://www.youtube.com/watch?v=4iNRX6Y0yS0"
 
 
-# Worst case scenario it returns  No Video Information Found
-except:
-    # Returns an no video information found dictionary
-    print("No Video Information Found.")
-    result = {'title': "No Video Information Found",
-              'views': "No Video Information Found",
-              'description': "No Video Information Found",
-              'date_published': "No Video Information Found",
-              'likes': "No Video Information Found",
-              'dislikes': "No Video Information Found"}
-    channel_tag = 'No Video Information Found'
-    channel_name = 'No Video Information Found'
-    channel_url = 'No Video Information Found'
-    channel_subscribers = 'No Video Information Found'
-    result['channel'] = {'name': channel_name, 'url': channel_url, 'subscribers': channel_subscribers}
+def test_video_info_url(get_video_info_list):
+    """
+    Function evaluates the list generated from get_video_info() against a blank one to see any missing info and
+    prints out results
+    """
+    print("Testing URL...")
+    # Blank result for logical testing
+    result_blank = {'title': "No Video Information Found",
+                    'views': "No Video Information Found",
+                    'description': "No Video Information Found",
+                    'date_published': "No Video Information Found",
+                    'likes': "No Video Information Found",
+                    'dislikes': "No Video Information Found"}
+    blank_channel_tag = 'No Video Information Found'
+    blank_channel_name = 'No Video Information Found'
+    blank_channel_url = 'No Video Information Found'
+    blank_channel_subscribers = 'No Video Information Found'
+    result_blank['channel'] = {'name': blank_channel_name, 'url': blank_channel_url,
+                               'subscribers': blank_channel_subscribers}
 
-# Blank result for logical testing
-result_blank = {'title': "No Video Information Found",
-                'views': "No Video Information Found",
-                'description': "No Video Information Found",
-                'date_published': "No Video Information Found",
-                'likes': "No Video Information Found",
-                'dislikes': "No Video Information Found"}
-channel_tag = 'No Video Information Found'
-channel_name = 'No Video Information Found'
-channel_url = 'No Video Information Found'
-channel_subscribers = 'No Video Information Found'
-result['channel'] = {'name': channel_name, 'url': channel_url, 'subscribers': channel_subscribers}
+    print("#----------------------- Results ------------------------#")
+    # Checks to see if the blank result object was returned
+    if get_video_info_list == result_blank:
+        print("Blank result object returned")
+
+    # Prints out each result
+    for item in get_video_info_list:
+        if get_video_info_list[item] == "Not Found (Perhaps Hidden)":
+            print(f"{item}: Missing - X")
+        else:
+            print(f"{item}: Found - V")
+
+    for channel_descriptor in get_video_info_list['channel']:
+        if get_video_info_list['channel'][channel_descriptor] == "Not Found (Perhaps Hidden)":
+            print(f"channel {channel_descriptor}: Missing - X")
+        else:
+            print(f"channel {channel_descriptor}: Found - V")
+
+    print("Testing Complete!")
 
 
-print("#----------------------- Results ------------------------#")
-# Checks to see if the blank result object was returned
-if result == result_blank:
-    print("Blank result object tripped.")
-
-# Prints out each result
-for item in result:
-    if item == "Not Found (Perhaps Hidden)":
-        print(f"{item} Missing - X")
-    else:
-        print(f"{item} Found - V")
-
-print("Testing Complete!")
+if __name__ == '__main__':
+    # Test
+    test_video_info_url(get_video_info(url))
